d413ac0b42daf25fa154131856db3c65c39e9e44
CodeChef <joetomsim@gmail.com>
2024-04-17 21:45:30 -0500

    set up API key

---
+++ logic/script.js
@@ -0,0 +1,7 @@
+const APIKey = '65ea7f0bc4bc6877c37099a77bad2248';
+let city;
+const requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
+
+fetch (requestURL)
+
+
