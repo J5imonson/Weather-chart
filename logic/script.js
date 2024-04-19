51bd47d5f98a3d787dffd7475a85705f986fe747
CodeChef <joetomsim@gmail.com>
2024-04-18 23:11:07 -0500

    worked on fetch

--- logic/script.js
+++ logic/script.js
@@ -1,7 +1,18 @@
 const APIKey = '65ea7f0bc4bc6877c37099a77bad2248';
-let city;
-const requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
+const cityName = `https://api.openweathermap.org/data/2.5/forecast?lat=${ lat }&lon=${ lon }&appid=${ APIKey }`;
+const coordinates = `http://api.openweathermap.org/geo/1.0/direct?q=${ city }&appid=${ APIKey }`;
 
-fetch (requestURL)
+const city = $('#city')
 
+//finds a city's coordinates
+function citySelect() {
+  fetch(coordinates)
+    .then(function (response) {
+      return response.json();
+    })
+  .then(function (data) {
+      console.log(data)
+    });
+}
 
+city.on('click', citySelect());
